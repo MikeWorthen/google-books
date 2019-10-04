e4e02e7511b55e752d67d103d5525db688b37aba
mworth <mikeworthen55@gmail.com>
2019-10-03 23:15:37 -0600

    connection to api is wokring, trying to get DB set

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// const routes = require("./routes");
+const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Add routes, both API and view
-// app.use(routes);
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books")
@@ -22,8 +22,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books")
     .catch(err => console.log(err));
 
 // Start the API server
-// app.listen(PORT, function() {
-//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-// });
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
 
 app.listen(PORT, console.log("http://localhost:" + PORT,));
